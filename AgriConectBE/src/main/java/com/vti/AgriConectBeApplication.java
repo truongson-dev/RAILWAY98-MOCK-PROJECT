5ad54618e00ff720f56df26b82403681e4ff8e59
NguyenThanhSon2002 <nguyenthanhsonyb2002@gmail.com>
2026-07-25 12:39:49 +0700

    Upadate folder 25072026

--- AgriConectBE/src/main/java/com/vti/AgriConectBeApplication.java
+++ AgriConectBE/src/main/java/com/vti/AgriConectBeApplication.java
@@ -1,13 +1,13 @@
-package com.agriconnect;
+package com.vti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AgriConnectApplication {
+public class AgriConectBeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AgriConnectApplication.class, args);
+		SpringApplication.run(AgriConectBeApplication.class, args);
 	}
 
 }
