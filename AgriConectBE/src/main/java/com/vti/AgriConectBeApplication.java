fdda28c8d6cda2158404c2cea2a5949b3dfe60f6
user <user@gmail.com>
2026-07-28 17:24:39 +0700

    add data to the .env file and create an .env.example file

--- AgriConectBE/src/main/java/com/vti/AgriConectBeApplication.java
+++ AgriConectBE/src/main/java/com/vti/AgriConectBeApplication.java
@@ -3,11 +3,17 @@ package com.vti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class AgriConectBeApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(AgriConectBeApplication.class, args);
 	}
 
 }
+
--- AgriConectBE/src/test/java/com/vti/AgriConectBeApplicationTests.java
+++ AgriConectBE/src/test/java/com/vti/AgriConectBeApplicationTests.java
@@ -1,9 +1,7 @@
 package com.vti;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AgriConectBeApplicationTests {
 
 	@Test
